7bf90d1c2fa4281c8784c34b142d37fbe92aea92
Adrian Brady <ajbrady999@protonmail.com>
2024-05-08 18:28:00 -0500

    add errors to parser

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -6,6 +6,19 @@ import (
 	"testing"
 )
 
+func checkParserErrors(t *testing.T, p *Parser) {
+	errors := p.Errors()
+	if len(errors) == 0 {
+		return
+	}
+
+	t.Errorf("parser has %d errors", len(errors))
+	for _, msg := range errors {
+		t.Errorf("parser error: %q", msg)
+	}
+	t.FailNow()
+}
+
 func TestLetStatements(t *testing.T) {
 	input := `
 let x = 5;
